56adb5fd083c2d5b1728f5a134f6b5e13a0ec94b
Putseyeu <putseyeu.a.d@gmail.com>
2022-03-12 20:56:33 +0100

    step1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int n = random.Next(1, 28);
+            Console.WriteLine(n);
+            int totalNumber = 0;
+            int maxNumber = 1000;
+            int minNumber = 100;
+
+            for (int i = minNumber; i < maxNumber; i++)
+            {
+                int c = i;
+                while (c >= n)
+                {
+                    c -= n;
+                    if (c == 0)
+                    {
+                        totalNumber += 1;
+                    }
+                }
+            }
+            Console.WriteLine($"Количество трехзначных натуральных чисел, которые кратны {n} равно {totalNumber}");
+        }
+    }
+}
